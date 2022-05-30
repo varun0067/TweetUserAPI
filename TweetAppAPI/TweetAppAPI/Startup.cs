a1cd2b4238288dbe71a56b2ae51709b147abf2d9
varun0067 <varunganesh006@gmail.com>
2022-05-30 16:59:47 +0530

    deploy connection string change

--- TweetAppAPI/TweetAppAPI/Startup.cs
+++ TweetAppAPI/TweetAppAPI/Startup.cs
@@ -79,12 +79,10 @@ namespace TweetAppAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TweetAppAPI v1"));
-            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TweetAppAPI v1"));
 
             app.UseCors(x => x
             .AllowAnyOrigin()
--- TweetAppUserAPI/TweetAppUserAPI/Startup.cs
+++ TweetAppUserAPI/TweetAppUserAPI/Startup.cs
@@ -49,12 +49,11 @@ namespace TweetAppUserAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TweetAppUserAPI v1"));
-            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TweetAppUserAPI v1"));
+
 
             app.UseCors(x => x
             .AllowAnyOrigin()
